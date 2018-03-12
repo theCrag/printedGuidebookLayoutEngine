324471d156e43e4b88f52ef5fc6959da7eef519c
Gerhard Fabian Hirschfeld <gfhirschfeld@openwt.com>
2018-03-12 14:00:10 +0100

    fix tests and remove database stuff

--- test/e2e/utils/bootstrap.ts
+++ test/e2e/utils/bootstrap.ts
@@ -1,19 +1,16 @@
 import { Application } from 'express';
 import * as http from 'http';
 import { bootstrapMicroframework } from 'microframework-w3tec';
-import { Connection } from 'typeorm/connection/Connection';
 
 import { eventDispatchLoader } from '../../../src/loaders/eventDispatchLoader';
 import { expressLoader } from '../../../src/loaders/expressLoader';
 import { homeLoader } from '../../../src/loaders/homeLoader';
 import { iocLoader } from '../../../src/loaders/iocLoader';
 import { winstonLoader } from '../../../src/loaders/winstonLoader';
-import { typeormLoader } from '../utils/typeormLoader';
 
 export interface BootstrapSettings {
     app: Application;
     server: http.Server;
-    connection: Connection;
 }
 
 export const bootstrapApp = async (): Promise<BootstrapSettings> => {
@@ -22,7 +19,6 @@ export const bootstrapApp = async (): Promise<BootstrapSettings> => {
             winstonLoader,
             iocLoader,
             eventDispatchLoader,
-            typeormLoader,
             expressLoader,
             homeLoader,
         ],
@@ -30,6 +26,5 @@ export const bootstrapApp = async (): Promise<BootstrapSettings> => {
     return {
         app: framework.settings.getData('express_app') as Application,
         server: framework.settings.getData('express_server') as http.Server,
-        connection: framework.settings.getData('connection') as Connection,
     } as BootstrapSettings;
 };
