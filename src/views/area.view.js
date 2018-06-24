c0520bbfe1c348fab5a4d7d7184390af56278dfb
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-06-24 14:23:57 +0200

    :lipstick: Add route layout

--- src/views/area.view.js
+++ src/views/area.view.js
@@ -43,11 +43,23 @@ export const routeItem = (routeItem, areaId, index) =>
           <div class="route__header__name">
             ${routeItem.name}
           </div>
-          <div class="route__header__grade">
-            ${routeItem.grade}
-          </div>
-          <div class="route__header__style">
-            ${routeItem.style}
+          <div class="route__header__extras">
+            ${routeItem.grade ? `
+            <div class="route__header__grade">
+              ${routeItem.grade}
+            </div>
+            `: ''}
+            ${routeItem.style ? `
+            <div class="route__header__style">
+              ${routeItem.style}
+            </div>
+            `: ''}
+            <div class="route__header__distance">
+            ${routeItem.bolts ? `<span class="clip"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 18" width="11" height="18"><path d="m8,17 0,-2 c1,0 2,-1 2,-2 l0,-9 c0,-2 -2,-3 -4,-3 -4,0 -5,3 -5,5 0,1 1,1.5 1.5,1.5 0.5,1.5 0.5,2.5 1,4 C2,13.4 5,15.4 6,15.4 l0,2 z M3,6.4 c0,0 0,-3 3,-3 1,0 2,0 2,2 l0,7 c0,1 -1,1 -2,1 -1,0 -2,-2 -2,-2 l -1,-5" style="fill:currentColor"></path></svg></span>` : ''}
+              ${routeItem.bolts ? routeItem.bolts : ''}
+              ${routeItem.displayHeight && routeItem.bolts ? ',' : ''}
+              ${routeItem.displayHeight ? routeItem.displayHeight : ''}
+            </div>
           </div>
         </div>
         <div class="route__body">
