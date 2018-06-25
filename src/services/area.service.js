0b012fd0621c2129dc649171a513e418b4433788
Gabriel Brunner <gaebu.brunner@gmail.com>
2018-06-25 11:42:58 +0200

    handle widows in descriptions

--- src/services/area.service.js
+++ src/services/area.service.js
@@ -1,6 +1,8 @@
-import * as jsonData from '../data/umbrio.json';
+// import * as jsonData from '../data/umbrio.json';
 // import * as jsonData from '../data/central-park.json';
+import * as jsonData from '../data/central-park-longDesc.json';
 import { Area } from '../models/area.model';
+import $ from "jquery";
 
 let rootArea;
 
