b180d94f594067337cbdfc097b78b23e163970f5
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-07-01 11:20:48 +0200

    :heavy_plus_sign: Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "globals": {
+    "process": true,
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2,
+      { "SwitchCase": 1 }
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "debugger": true,
+  }
+};
