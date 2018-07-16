4ab5fc01748290c64bad56162e978e646203bf1b
hirsch88 <gery.hirschfeld@w3tec.ch>
2018-07-16 09:47:58 +0200

    :bulb: Documenting source code

--- src/models/description.model.js
+++ src/models/description.model.js
@@ -1,7 +1,21 @@
+/**
+ * Description block of an area. Normally in the markdown format,
+ * but it will be replaced with the html.
+ */
 export class Description {
 
+  /**
+   * @param {JsonObject} jsonDescription
+   */
   constructor(jsonDescription) {
+    /**
+     * The name of the description block.
+     */
     this.name = jsonDescription.name;
+    /**
+     * At first this is real markdown, but it will
+     * be replace with html.
+     */
     this.markdown = jsonDescription.markdown;
   }
 
--- src/models/image-styles.js
+++ src/models/image-styles.js
@@ -1,3 +1,7 @@
+/**
+ * Image styles are the possible format a image could have in
+ * the booklet.
+ */
 export const FULL_PAGE = 0;
 export const FULL_WIDTH = 1;
 export const COL_WIDTH = 2;
